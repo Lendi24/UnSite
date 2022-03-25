2d21e0e46717f88ab1bd2357b2c1705619028ebe
Lendi24 <Lendic24@hotmail.com>
2022-03-25 17:23:59 +0100

    changing some text if login or signup

--- js/main-page-spa.js
+++ js/main-page-spa.js
@@ -1,14 +1,17 @@
 function EnterAuthMode(mode) {
     var elem = document.getElementById("auth-popup"); 
+    var title = document.getElementById("auth-title"); 
+
     switch (mode) {
         case 'login':
             elem.classList.add("unsite-anim-show");
+            title.innerText = "Login"
             break;
 
         case 'signu':
             elem.classList.add("unsite-anim-show");
+            title.innerText = "Signup"
             break;
-        
     
         default:
             elem.classList.remove("unsite-anim-show");
