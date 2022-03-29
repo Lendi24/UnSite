206057926f8c1f4ccf44b71a7cfda817966bf92b
Fojii <tiger.sebbe@hotmail.com>
2022-03-29 11:24:42 +0200

    Timer sends you to randomly chosen link

--- js/rick.js
+++ js/rick.js
@@ -1,5 +1,7 @@
+var trollLinks = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.youtube.com/watch?v=BsIa_LKojJI"]
+var randLink = Math.random
 var rand = Math.random() * 100;
 
 setTimeout(function() {
-    window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-}, rand * 1000);
+    window.location.replace(trollLinks[Math.floor(Math.random()*trollLinks.length)]);
+},rand * 1000);
