97411fb65f72c54740c7cfe6387bd4d1373f1125
Fojii <tiger.sebbe@hotmail.com>
2022-03-30 12:03:05 +0200

    Added more troll links

--- js/rick.js
+++ js/rick.js
@@ -1,7 +1,7 @@
-var trollLinks = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.youtube.com/watch?v=BsIa_LKojJI"]
+var trollLinks = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.youtube.com/watch?v=BsIa_LKojJI","https://www.youtube.com/watch?v=boPyHl3iptQ","https://www.youtube.com/watch?v=Cm0qaXi9THA","https://www.youtube.com/watch?v=71BCNq5tLaU","https://www.youtube.com/watch?v=12rT3uotaqk"]
 var randLink = Math.random
 var rand = Math.random() * 100;
 
 setTimeout(function() {
-    window.location.replace(trollLinks[Math.floor(Math.random()*trollLinks.length)]);
+    window.location.href = trollLinks[Math.floor(Math.random()*trollLinks.length)];
 },rand * 1000);
