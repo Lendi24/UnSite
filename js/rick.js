598ac6599013c06956a43447d7aea8e7133583ed
Fojii <tiger.sebbe@hotmail.com>
2022-03-30 12:26:45 +0200

    Timer restarts if user goes back to page

--- js/rick.js
+++ js/rick.js
@@ -1,7 +1,11 @@
 var trollLinks = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ","https://www.youtube.com/watch?v=BsIa_LKojJI","https://www.youtube.com/watch?v=boPyHl3iptQ","https://www.youtube.com/watch?v=Cm0qaXi9THA","https://www.youtube.com/watch?v=71BCNq5tLaU","https://www.youtube.com/watch?v=12rT3uotaqk"]
 var randLink = Math.random
 var rand = Math.random() * 100;
+Timer();
 
-setTimeout(function() {
-    window.location.href = trollLinks[Math.floor(Math.random()*trollLinks.length)];
-},rand * 1000);
+function Timer() {
+    setTimeout(function() {
+        window.location.href = trollLinks[Math.floor(Math.random()*trollLinks.length)];
+    },rand * 1000);
+    Timer();
+}
