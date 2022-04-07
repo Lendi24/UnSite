85d38020792b3f1b742de9a248770bff9daaa9b2
Lendi24 <Lendic24@hotmail.com>
2022-04-07 10:56:31 +0200

    bru

--- js/main-page-spa.js
+++ js/main-page-spa.js
@@ -1,16 +1,49 @@
+    //Change if login or signup
+    var elem;
+    var title;
+    var agreeText;
+
+    //Input to hook
+    var agreeBox;
+    var usrnme;
+    var passwd;
+
+
+window.onload = function() {
+    //Change if login or signup
+    elem = document.getElementById("auth-popup"); 
+    title = document.getElementById("auth-title"); 
+    agreeText = document.getElementById("auth-agree-text");
+    
+    //input to hook
+    submButton = document.
+    agreeBox = document.getElementById("auth-agree-box");
+    usrnme = document.getElementById("usr-name");
+    passwd = document.getElementById("paswd");   
+};
+
+function ChangedCheck(checked) {
+    console.log(checked);
+    disabled 
+}
+
 function EnterAuthMode(mode) {
-    var elem = document.getElementById("auth-popup"); 
-    var title = document.getElementById("auth-title"); 
 
     switch (mode) {
         case 'login':
             elem.classList.add("unsite-anim-show");
-            title.innerText = "Login"
+            title.innerText = "Login";
+            agreeBox.checked = true;
+            agreeBox.hidden = true;
+            agreeText.hidden = true;
             break;
 
         case 'signu':
             elem.classList.add("unsite-anim-show");
-            title.innerText = "Signup"
+            title.innerText = "Signup";
+            agreeBox.checked = false;
+            agreeBox.hidden = false;
+            agreeText.hidden = false;
             break;
     
         default:
