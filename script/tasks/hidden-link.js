fbdef4edbe0c2f7297546d285211ea6122b3803f
Ali-NTI-1126 <ali.rezai@elev.ga.ntig.se>
2022-09-08 10:48:10 +0200

    msg

--- script/tasks/hidden-link.js
+++ script/tasks/hidden-link.js
@@ -1,17 +1,24 @@
-$(document).ready(function () {
-    animateDiv(".box");
+$(document).ready(function(){
+  animateDiv(".box");
+
 });
-function makeNewPosition() {
-    let h = $(window).height() - 50;
-    let w = $(window).width() - 50;
-    let nh = Math.floor(Math.random() * h);
-    let nw = Math.floor(Math.random() * w);
-    return [nh, nw];
+
+function makeNewPosition(){
+  
+  let h = $(window).height() - 50;
+  let w = $(window).width() - 50;
+  
+  let nh = Math.floor(Math.random() * h);
+  let nw = Math.floor(Math.random() * w);
+  
+  return [nh,nw];    
+  
 }
-function animateDiv(myclass) {
-    let newq = makeNewPosition();
-    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000, function () {
-        animateDiv(myclass);
-    });
-}
-;
+
+function animateDiv(myclass){
+  let newq = makeNewPosition();
+  $(myclass).animate({ top: newq[0], left: newq[1] }, 1000,   function(){
+    animateDiv(myclass);        
+  });
+  
+};
