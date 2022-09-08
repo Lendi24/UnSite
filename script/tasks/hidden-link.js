8fef76b0f0b20e58cb049ce9b744cf42ff10cfc9
Ali-NTI-1126 <ali.rezai@elev.ga.ntig.se>
2022-09-08 10:41:15 +0200

    init commit

---
+++ script/tasks/hidden-link.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    animateDiv(".box");
+});
+function makeNewPosition() {
+    let h = $(window).height() - 50;
+    let w = $(window).width() - 50;
+    let nh = Math.floor(Math.random() * h);
+    let nw = Math.floor(Math.random() * w);
+    return [nh, nw];
+}
+function animateDiv(myclass) {
+    let newq = makeNewPosition();
+    $(myclass).animate({ top: newq[0], left: newq[1] }, 1000, function () {
+        animateDiv(myclass);
+    });
+}
+;
