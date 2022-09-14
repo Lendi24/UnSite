ca892970477ca9581e3c7ed5027660721a29bb7b
Ali-NTI-1126 <ali.rezai@elev.ga.ntig.se>
2022-09-14 14:58:37 +0200

    added wall of buttons base

---
+++ script/tasks/wall-of-buttons.js
@@ -0,0 +1,35 @@
+function buttonLogic(elem, isCorrect) {
+    if (isCorrect) {
+        //is correct
+    }
+    else {
+        elem.style.transition = "1s";
+        elem.style.transform = "scale(2)";
+        elem.style.filter = "opacity(0%)";
+        //elem.classList.add("is-danger");
+        //elem.remove();
+    }
+}
+const div = document.getElementById("buttons");
+/*
+div.innerHTML += "<a class='button is-success' onclick='buttonLogic(this, false)'>Test</a>";
+div.innerHTML += "<a class='button is-success' onclick='buttonLogic(this, false)'>Test3</a>";
+*/
+let amountOfButtons = 30;
+let indexOfCorrectbutton = Math.floor(Math.random() * amountOfButtons);
+for (let i = 0; i < amountOfButtons; i++) {
+    if (i != indexOfCorrectbutton) {
+        div.innerHTML += "<a class='button m-1 is-success' onclick='buttonLogic(this, false)'>Next</a>";
+    }
+    else {
+        div.innerHTML += "<a class='button m-1 is-success' onclick='buttonLogic(this, true)'>Next</a>";
+    }
+}
+/*
+function limit() {
+    let div = document.getElementById("buttons");
+    if (div.screen.height <= inner.height) {
+      x.style.display = "block";
+    }
+  }
+*/
