7a5d4358cae654cd78a53ba53af2d39e3159793c
Ali-NTI-1126 <ali.rezai@elev.ga.ntig.se>
2022-09-19 09:06:36 +0200

    init upload

---
+++ script/tasks/quiz.js
@@ -0,0 +1,38 @@
+const nextButton = document.getElementById("nextButtons");
+const questionContainerElement = document.getElementById("questionContainer");
+const questionElement = dokument.getElementById("quistion")
+const awnserbuttonsElement = dokument.getElementById("awnsers")
+
+nextButton.addEventListener("click", () => {
+NextQuestion
+})
+
+
+const question = document.getElementById("question");
+function question(question) {
+
+}
+
+
+function showQuestion() {
+
+}
+
+function NextQuestion() {
+
+}
+
+
+const questions = [
+    {
+        question : "question nummber 1? ",
+        awnnsers : [
+            { text: "Ali", correct: false},
+            { text: "Fabian", correct: false},
+            { text: "Emanuel", correct: false},
+            { text: "Dan", correct: true}
+
+        ]
+
+    }
+]
