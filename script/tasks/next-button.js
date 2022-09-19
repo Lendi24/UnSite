65a07eeb1597502322914b69bee3a3350947138d
Lendi24 <Lendic24@hotmail.com>
2022-09-19 11:29:53 +0200

    Added JS logic

--- script/tasks/next-button.js
+++ script/tasks/next-button.js
@@ -1,5 +1,21 @@
 class TaskNextButton extends TaskObj {
     taskLogic() {
-        console.log(`[Warning](TaskObj) No task logic has been difinend for "${this.name}" task`);
+        document.getElementById("next").onclick = function () {
+            const bar = document.getElementById("bar");
+            const con = document.getElementById("button-contain");
+            bar.value += 8;
+            if (bar.value >= 100) {
+                document.getElementById("next-task-button").click();
+            }
+            else {
+                if (Math.random() <= 0.33) {
+                    con.classList.toggle("is-flex-direction-row-reverse");
+                }
+            }
+        };
+        document.getElementById("prev").onclick = function () {
+            const bar = document.getElementById("bar");
+            bar.value = 0;
+        };
     }
 }
