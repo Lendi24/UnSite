774d286bcc02c148d32096ccf100a1c0f4e271e7
Lendi24 <Lendic24@hotmail.com>
2022-09-19 09:31:16 +0200

    Cleaned code

--- script/tasks/wall-of-buttons.ts
+++ script/tasks/wall-of-buttons.ts
@@ -1,46 +1,30 @@
-function buttonLogic(elem, isCorrect) {
-    if (isCorrect) {
-        //is correct
-    }
-    else {
-        elem.style.transition = "1s";
-        elem.style.transform = "scale(2)";
-        elem.style.filter = "opacity(0%)";
-
-        //elem.classList.add("is-danger");
-        
-        //elem.remove();
-    }
-}
-
-const div = document.getElementById("buttons");
-/*
-div.innerHTML += "<a class='button is-success' onclick='buttonLogic(this, false)'>Test</a>";
-div.innerHTML += "<a class='button is-success' onclick='buttonLogic(this, false)'>Test3</a>";
-*/
 let amountOfButtons = 30;
 let indexOfCorrectbutton = Math.floor(Math.random() * amountOfButtons);
+const buttonContain = document.getElementById("buttons");
 
 for (let i = 0; i < amountOfButtons; i++) {
     
+    let button = document.createElement('a');
+    button = buttonContain.appendChild(button);
+    button.classList.add("button");
+    button.classList.add("is-success");
+    button.classList.add("m-1");
+    button.innerHTML = "Click me"
+
     if (i!=indexOfCorrectbutton) {
-        div.innerHTML += "<a class='button m-1 is-success' onclick='buttonLogic(this, false)'>Next</a>";
+        button.onclick = function(){
+            button.style.transition = "1s";
+            button.style.transform = "scale(2)";
+            button.style.filter = "opacity(0%)";
+        }
     }
     
     else {
-        div.innerHTML += "<a class='button m-1 is-success' onclick='buttonLogic(this, true)'>Next</a>";  
+        button.onclick = function(){
+            document.getElementById("next-task-button").click();
+        }
     }
 }
-/*
-function limit() {
-    let div = document.getElementById("buttons");
-    if (div.screen.height <= inner.height) {
-      x.style.display = "block";
-    } 
-  }
-*/
-
-
 
 
 
